b5b47bad61a542f1fe307d689bfcf79a91cebe3a
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2022-02-14 19:51:56 +0100

    FEAT auth login

---
+++ components/Login.js
@@ -0,0 +1,19 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export function Login() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Hola {session.user.email} <br />
+        <button onClick={() => signOut()}>Logout</button>
+      </>
+    )
+  }
+  return (
+    <>
+      Inicia sesión <br />
+      <button onClick={() => signIn('github')}>Login</button>
+    </>
+  )
+}
