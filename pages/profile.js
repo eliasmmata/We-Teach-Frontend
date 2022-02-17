7190dc5f57e37d91ac106043d9c501932af2882f
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2022-02-17 17:53:36 +0100

    github data

--- pages/profile.js
+++ pages/profile.js
@@ -1,12 +1,17 @@
 import { Container } from "../components/Container"
 import { useSession, signIn, signOut } from "next-auth/react"
+import Image from "next/image";
 
 export default function Profile() {
     const { data: session } = useSession();
     console.log(session)
     return (
         <Container>
-            Hola
+            <p>{user.email}</p>
+            <p>{user.name}</p>
+            <span className={styles.logo}>
+                <Image src={user.image} alt="profile pic" width={72} height={16} />
+            </span>
         </Container>
     )
 }
