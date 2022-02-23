1569f0db4666e8bea5c75995a7029dee2b3683f8
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2022-02-23 18:07:33 +0100

    error

--- models/userModel.js
+++ models/userModel.js
@@ -7,6 +7,8 @@ const UserSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique : true,
+        required : true,
     },
     password: {
         type: String,
