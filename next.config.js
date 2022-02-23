fb7fa2e1008d5e36c8cd8e42c3716d6476c84233
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2022-02-23 16:53:23 +0100

    test login

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   images: {
-    domains: ['avatars.githubusercontent.com','lh3.googleusercontent.com'],
+    domains: ['avatars.githubusercontent.com','lh3.googleusercontent.com', 'i.stack.imgur.com'],
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
