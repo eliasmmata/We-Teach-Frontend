abeccea50eba80166e9a43777f186e9c752b0810
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2022-02-23 20:24:45 +0100

    test

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,12 @@ import 'primeicons/primeicons.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/tailwind.css';
 
+import { useEffect } from "react";
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
