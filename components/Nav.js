efdc9005a9ea0a6c31fcf1dac514f8aeaf662b76
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2022-02-24 03:37:37 +0100

    test user

--- components/Nav.js
+++ components/Nav.js
@@ -9,7 +9,7 @@ export function Nav() {
     const toggleOpen = () => {
         setDropdown(!dropdown);
         setDropdownMenu(false);
-        console.log(session)
+        console.log(session.user.name)
     }
     const toggleOpenMenu = () => {
         setDropdownMenu(!dropdownMenu);
@@ -36,12 +36,7 @@ export function Nav() {
                             id="dropdown"
                             style={{ transform: `translate(-40%, 60%)` }}>
                             <div className="py-3 px-4">
-                                {session.user.name != undefined
-                                ?
-                                <span className="block text-sm text-gray-900 dark:text-white">{session.user.name}</span>
-                                :
                                 <span className="block text-sm text-gray-900 dark:text-white">Welcome</span>
-                                }
                                 {session && <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">{session.user.email}</span>}
                             </div>
                             <ul className="py-1" aria-labelledby="dropdown" style={{ padding: `0.5rem` }}>
