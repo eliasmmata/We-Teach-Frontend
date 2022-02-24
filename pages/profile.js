4f69b9f90c65d1d911dbf3a4bcef30f9ea7086af
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2022-02-24 03:25:43 +0100

    end

--- pages/profile.js
+++ pages/profile.js
@@ -1,13 +1,10 @@
 import { Container } from "../components/Container"
-import { useSession, signIn, signOut } from "next-auth/react"
-import Image from "next/image";
 import { Nav } from "../components/Nav";
 
 export default function Profile() {
-    const { data: session } = useSession();
     return (
-        <Container session={session}>
-            <Nav session={session}/>
+        <Container>
+            <Nav/>
         </Container>
     )
 }
