f9121e7b4d7ff28fea6680d8ca9ef34d45edaf54
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2022-02-24 18:23:07 +0100

    login page OK

--- components/Login.js
+++ components/Login.js
@@ -1,9 +1,10 @@
 import { useSession, signIn, signOut, getCsrfToken } from "next-auth/react"
 import Image from "next/image";
 import Router from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Footer } from "./Footer";
 import { Nav } from "./Nav";
+import Typewriter from 'typewriter-effect';
 
 
 export function Login({ csrfToken }) {
@@ -45,15 +46,51 @@ export function Login({ csrfToken }) {
       return Router.push("/")
     }
   }
-
   if (!session) {
     return (
       <>
-        <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8" style={{minHeight:`92vh`}}>
+        <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8" style={{ minHeight: `92vh` }}>
           <div className="max-w-md w-full space-y-8">
             <div>
-              <h2 className="text-center text-5xl font-extrabold text-gray-900">We Teach Frontend</h2>
-              <h3 className="mt-4 text-center text-3xl font-bold text-gray-600">Crea tu cuenta</h3>
+              {/*  <h2 className="text-center text-5xl font-extrabold text-gray-900">
+                <div className="title">{state.title}</div>
+                <div className="titleTwo">{state.titleTwo}</div>
+                <div className="titleThree">{state.titleThree}</div>
+              </h2> */}
+              <div className="text-center text-4xl font-extrabold text-900" style={{ color: `#2B193D` }}>
+                <Typewriter
+                  options={{
+                    autoStart: true,
+                    loop: true,
+                    delay: 'natural',
+                    deleteSpeed: 'natural',
+                    pauseFor: 3000,
+                    strings: [
+                      "W T F",
+                      "We Teach Frontend",
+                    ],
+                  }}
+                />
+              </div>
+              <div className="text-center text-3xl font-extrabold text-900 mt-4" style={{ color: `#C5979D` }}>
+                <Typewriter
+                  options={{
+                    autoStart: true,
+                    loop: true,
+                    delay: 80,
+                    deleteSpeed: 60,
+                    strings: [
+                      "React & NextJs",
+                      "CSS & SASS",
+                      "TypeScript",
+                      "Tailwind",
+                      "Bootstrap",
+                      "¡ Y mucho más !",
+                    ],
+                  }}
+                />
+              </div>
+              <h3 className="mt-4 text-center text-3xl font-bold text-600" style={{ color: `#2B193D` }}>Crea tu cuenta</h3>
               {/* <p className="mt-2 text-center text-sm text-gray-600">
                 Or
                 <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500"> start your 14-day free trial </a>
@@ -80,55 +117,63 @@ export function Login({ csrfToken }) {
                 </div>
 
                 <div className="text-sm">
-                  <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500"> ¿Olvidaste tu contraseña? </a>
+                  <a href="#" className="font-medium text--600 hover:text-500" style={{ color: `#C5979D` }}> ¿Olvidaste tu contraseña? </a>
                 </div>
               </div>
 
               <div>
-                <button onClick={(e) => signInUser(e)} className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <button onClick={(e) => signInUser(e)} className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" style={{ background: `#2C365E` }}>
                   <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                    <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <svg className="h-5 w-5 text-indigo-100 group-hover:text-indigo-200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                       <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
                     </svg>
                   </span>
                   Inicia Sesión
                 </button>
-                <button onClick={(e) => signUpUser(e)} className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <button onClick={(e) => signUpUser(e)} className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" style={{ background: `#484D6D` }}>
                   <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                    <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                      <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+                    <svg className="h-5 w-5 text-indigo-100 group-hover:text-indigo-200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path d="M3 12v3c0 1.657 3.134 3 7 3s7-1.343 7-3v-3c0 1.657-3.134 3-7 3s-7-1.343-7-3z" />
+                      <path d="M3 7v3c0 1.657 3.134 3 7 3s7-1.343 7-3V7c0 1.657-3.134 3-7 3S3 8.657 3 7z" />
+                      <path d="M17 5c0 1.657-3.134 3-7 3S3 6.657 3 5s3.134-3 7-3 7 1.343 7 3z" />
                     </svg>
                   </span>
                   Registrarse
                 </button>
               </div>
             </form>
-            <div className="col-xs-12 center-xs .d-xl-flex d-sm-flex justify-content-between" style={{gap: `1rem`}}>
-              <button onClick={() => signIn('github')} id="github-btn" className="btn btn-default btn-raised s-btn-sm p-left-xs-40 p-right-xs-40"><i className="pi pi-github" style={{ 'fontSize': '1em', marginRight: `5px` }}></i>Log in with GitHub</button>
-              <button onClick={() => signIn('google')} id="google-btn" className="btn btn-default btn-raised s-btn-sm p-left-xs-40 p-right-xs-40"><i className="pi pi-google" style={{ 'fontSize': '1em', marginRight: `5px` }}></i>Log in with Google</button>
+            <div className="col-xs-12 center-xs .d-xl-flex d-sm-flex justify-content-between" style={{ gap: `1rem` }}>
+              <button onClick={() => signIn('github')} id="github-btn" className="btn btn-default btn-raised s-btn-sm p-left-xs-40 p-right-xs-40">
+                <svg style={{ 'fontSize': '1em', marginRight: `5px`, background: `white`, borderRadius: `50%` }} aria-hidden="true" className="svg-icon iconGitHub" width="18" height="18" viewBox="0 0 18 18"><path d="M9 1a8 8 0 0 0-2.53 15.59c.4.07.55-.17.55-.38l-.01-1.49c-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82a7.42 7.42 0 0 1 4 0c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48l-.01 2.2c0 .21.15.46.55.38A8.01 8.01 0 0 0 9 1Z" fill="#010101"></path></svg>
+                Log in with GitHub
+              </button>
+              <button onClick={() => signIn('google')} id="google-btn" className="btn btn-default btn-raised s-btn-sm p-left-xs-40 p-right-xs-40">
+                <svg style={{ 'fontSize': '1em', marginRight: `5px` }} aria-hidden="true" className="native svg-icon iconGoogle" width="18" height="18" viewBox="0 0 18 18"><path d="M16.51 8H8.98v3h4.3c-.18 1-.74 1.48-1.6 2.04v2.01h2.6a7.8 7.8 0 0 0 2.38-5.88c0-.57-.05-.66-.15-1.18Z" fill="#4285F4"></path><path d="M8.98 17c2.16 0 3.97-.72 5.3-1.94l-2.6-2a4.8 4.8 0 0 1-7.18-2.54H1.83v2.07A8 8 0 0 0 8.98 17Z" fill="#34A853"></path><path d="M4.5 10.52a4.8 4.8 0 0 1 0-3.04V5.41H1.83a8 8 0 0 0 0 7.18l2.67-2.07Z" fill="#FBBC05"></path><path d="M8.98 4.18c1.17 0 2.23.4 3.06 1.2l2.3-2.3A8 8 0 0 0 1.83 5.4L4.5 7.49a4.77 4.77 0 0 1 4.48-3.3Z" fill="#EA4335"></path></svg>
+                Log in with Google
+              </button>
             </div>
           </div>
         </div>
-        <footer className="copyright-area" style={{width:`100%`, position:`absolute`, bottom:`0`}}>
+        <footer className="copyright-area-login" style={{ width: `100%`, position: `absolute`, bottom: `0` }}>
           <div className="container">
             <div className="row">
               <div className="col-xl-12 col-lg-12 text-center text-lg-left">
-                <div className="copyright-text">
-
+                <div className="copyright-text" style={{ display: `flex`, justifyContent: `center` }}>
+                  <span>
+                    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                  </span>
+                  <svg style={{ margin: `0 1rem`, color: `#C5979D` }} xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clipRule="evenodd" />
+                  </svg>
                   <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    href="https://eliasmmataportfolio.netlify.app/"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    Powered by{" "}
-                    <span>
-                      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                    </span>
+                    Eliasmmata
                   </a>
                 </div>
-
               </div>
-
             </div>
           </div>
         </footer>
