24be9aae6d376f3fb8fd8f8ae5ffc0f945726832
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2022-02-24 03:00:53 +0100

    test nav OK

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,10 @@ import { SessionProvider } from 'next-auth/react';
 
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
-import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/tailwind.css';
-import { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
-/* import { useEffect } from "react"; */
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  useEffect(() => {
-    import('@themesberg/flowbite')
-  }, [])
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
--- pages/profile.js
+++ pages/profile.js
@@ -1,25 +1,13 @@
 import { Container } from "../components/Container"
 import { useSession, signIn, signOut } from "next-auth/react"
 import Image from "next/image";
+import { Nav } from "../components/Nav";
 
 export default function Profile() {
     const { data: session } = useSession();
-    if (!session) {
-        return (
-            <>
-                Inicia sesión <br />
-                <button onClick={() => signIn('github')}>Login</button>
-            </>
-        )
-    }
-
     return (
-        <Container>
-            <p>{session?.user?.name}</p>
-            <span className={styles.logo}>xs
-                <Image src={session?.user?.image} alt="profile pic" width={112} height={112} />
-            </span>
-            <p>{session?.user?.email}</p>
+        <Container session={session}>
+            <Nav session={session}/>
         </Container>
     )
 }
