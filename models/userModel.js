315362dc82eca789e874754c41ea6709f2db7dd3
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2022-02-24 03:47:47 +0100

    login OK

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
-        default: 'guest'
+        default: 'Guest'
     },
     email: {
         type: String,
