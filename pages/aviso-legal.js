9410cf526b56dd5b1a6b955df5392003ff4a6976
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2022-03-02 19:48:52 +0100

    politica privacidad

--- pages/aviso-legal.js
+++ pages/aviso-legal.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { Container } from "../components/Container";
 
-export default function Custom404() {
+export default function AvisoLegal() {
     const router = useRouter()
     return (
         <Container>
@@ -11,7 +11,7 @@ export default function Custom404() {
             >
                 <div className="md:px-20 md:py-10 lg:px-40 lg:py-20 bg-white rounded-md shadow-xl">
                     <div className="flex flex-col items-center p-3 ">
-                        <h1 className="font-bold text-spacecadet text-7xl md:text-9xl">Aviso Legal</h1>
+                        <h1 className="font-bold text-spacecadet text-2xl md:text-4xl">Aviso Legal</h1>
 
                         <h6 className="mb-2 text-2xl font-bold text-center text-tuscany md:text-3xl">
                             <span className="text-spacecadet">Coming</span> soon
