f8cca280a76c566bfdf05197357893c5ac185b9c
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2022-03-02 21:43:04 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,15 @@ export default function Home({ posts }) {
   if (session) {
     return (
       <>
-       <Script src="/scripts/instafeed.min.js" strategy="beforeInteractive" />
-       <Script id="show-banner">
+        <Container>
+          <LoginOk />
+          {/* Instagram no puede hacerse como componente por Next js no pasa props de fetch */}
+          <p className="bg-background-100 mx-auto pb-4 lg:pb-8  text-center text-darkpurple font-MontserratBold sm:w-100 xl:text-4xl text-3xl">Instagram</p>
+          <div id="instafeed-container" className="bg-background-100 pb-16">
+          </div>
+          <Footer />
+          <Script src="/scripts/instafeed.min.js" strategy="beforeInteractive" />
+          <Script id="show-banner">
             {`var userFeed = new Instafeed({
                 get: 'user',
                 target: "instafeed-container",
@@ -40,19 +47,39 @@ export default function Home({ posts }) {
             });
             userFeed.run();`}
           </Script>
-        <Container>
-          <LoginOk />
-          {/* Instagram no puede hacerse como componente por Next js no pasa props de fetch */}
-          <p className="bg-background-100 mx-auto pb-4 lg:pb-8  text-center text-darkpurple font-MontserratBold sm:w-100 xl:text-4xl text-3xl">Instagram</p>
-          <div id="instafeed-container" className="bg-background-100 pb-16">
-          </div>
-          <Footer />
         </Container>
       </>
     )
   }
   return (
     <>
+      <Script src="/scripts/instafeed.min.js" strategy="beforeInteractive" />
+      <Script id="show-banner">
+        {`var userFeed = new Instafeed({
+                get: 'user',
+                target: "instafeed-container",
+                resolution: 'low_resolution',
+                accessToken: 'IGQVJYcFVGWDYzS0Y3eWZAHM2N5U29rdzFfdk9PTE4wR1VGWTZAvYnUyZAWh0U3kySnR6T3dJVWZARaS0tOEdXLWFoT1FLaXlMUXUyRmMtWXJ3dE5MRm9US08wbl90X0pOZAUR4Tk5JMzFOOThQRzRkT1ZAtTwZDZD',
+                limit: 6,
+                template: '<div class="ig-li">' +
+                            '<ul class="ig-list">' +
+                                '<a href="{{link}}" target="_blank" rel="noreferrer">' +
+                                    '<img title="{{caption}}" src="{{image}}" />' +
+                                '</a>' +
+                                '<div class="text-ig-container">' +
+                                    '<p>{{caption}}</p>' +
+                                '</div>' +
+                                '<a href="{{link}}" class="button-link" target="_blank" rel="noreferrer">' +
+                                    '<button>' +
+                                    'Ir a Ig' +
+                                    '<i class="pi pi-instagram ml-4 my-auto"></i>' +
+                                    '</button>' +
+                                '</a>' +
+                            '</ul>' +
+                          '</div>'
+            });
+            userFeed.run();`}
+      </Script>
       <LoginForm />
     </>
   )
