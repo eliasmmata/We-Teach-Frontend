995282b1687a5e3dc9c628e6fee16a932cbaeebd
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2022-03-08 18:06:45 +0100

    loading

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
 
   return (
     <>
-      {status !== "authenticated"
+      {status === "authenticated"
         ? <ProgressSpinner />
         : <>
           <Container>
