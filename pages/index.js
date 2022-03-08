fcf7007e76bf969d6ccb2e5ddc81f8cbd76bcf25
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2022-03-08 17:19:11 +0100

    loading

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,13 @@ import LoginOk from "../components/LoginOk";
 import { Footer } from "../components/Footer";
 import { Instagram } from "../components/Instagram";
 
+import { ProgressSpinner } from 'primereact/progressspinner';
+
 import Script from 'next/script';
 
 
 export default function Home() {
-  const { data: session } = useSession()
+  const { data: session, status } = useSession()
   if (!session) {
     return (
       <>
@@ -25,6 +27,7 @@ export default function Home() {
     <>
       <Container>
         <Script src="/scripts/instafeed.min.js"></Script>
+        {status === "loading" && <ProgressSpinner />}
         <LoginOk />
         <Instagram />
         <Footer />
