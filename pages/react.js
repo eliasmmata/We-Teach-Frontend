0f0b2f655215dcd4ce59446740041bdeb4cd1dee
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2022-03-14 18:59:47 +0100

    root

--- pages/react.js
+++ pages/react.js
@@ -3,6 +3,7 @@ import { Container } from '../components/Container'
 import { Footer } from '../components/Footer'
 import { Nav } from '../components/Nav'
 
+
 export default function ReactPage({ resources }) {
     return (
         <>
