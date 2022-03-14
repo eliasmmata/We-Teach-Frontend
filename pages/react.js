8bee9c055488278341671eb2238f83f69179058e
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2022-03-14 19:29:52 +0100

    swr library axios to fech client side

--- pages/react.js
+++ pages/react.js
@@ -3,8 +3,19 @@ import { Container } from '../components/Container'
 import { Footer } from '../components/Footer'
 import { Nav } from '../components/Nav'
 
+import useSWR from 'swr';
+import axios from "axios";
+import { ProgressSpinner } from 'primereact/progressspinner';
 
-export default function ReactPage({ resources }) {
+
+const fetcher = url => axios.get(url).then(res => res.data)
+
+export default function ReactPage() {
+  const { data, error } = useSWR('api/resources', fetcher);
+
+    if (error) return <div>Failed to load</div>
+    if (!data) return <span style={{padding: `25vw 25vw`}}><ProgressSpinner /></span>
+
     return (
         <>
             <Container>
@@ -14,11 +25,11 @@ export default function ReactPage({ resources }) {
                 </p>
 
                 <div>
-                    {resources.map(resource => (
+                    {data.map(resource => (
                         <div key={resource.id}>
                             <h2>{resource.title}</h2>
                             <p>{resource.description}</p>
-                            <p>{resource.url}</p>
+                            <a href={resource.url} target="_blank" rel="noreferrer">{resource.url}</a>
                             {/* <ul>
                                 {book.authors.map(author => (
                                     <li key={author}>{author}</li>
@@ -36,7 +47,7 @@ export default function ReactPage({ resources }) {
     )
 }
 
-export async function getStaticProps() {
+/* export async function getStaticProps() {
     const API_URL = process.env.API_URL || 'http://localhost:3000'
     var res = await fetch(
         `${API_URL}/api/resources`,
@@ -59,3 +70,4 @@ export async function getStaticProps() {
     }
 }
 
+ */
